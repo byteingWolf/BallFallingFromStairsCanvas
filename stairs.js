708ea3ecefa70fd958b7fe8a999648592779ef04
wolf4fun <wilczek4fun@gmail.com>
2021-10-14 18:40:52 +0200

    testing animation

--- stairs.js
+++ stairs.js
@@ -12,7 +12,6 @@ let y = y_start;
 let i_start = 5;
 const amount = x + 5
 
-
 class Stair {
   constructor(x, y) {
     this.x = x;
@@ -20,7 +19,15 @@ class Stair {
   }
 };
 
-stairs = []
+class Ball {
+  constructor(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+  }
+};
+
+let stairs = []
 
 function drawStairsRow() {
   for (i = i_start; i <= x; i++) {
@@ -32,6 +39,9 @@ function drawStairsRow() {
   y++;
 }
 
+var circle_x = x_start*25+7.5
+var circle_y = (y_start-1) * 25 + 10
+
 const drawStarisInterval = setInterval(() => {
   drawStairsRow()
   if (x === amount) {
@@ -40,72 +50,96 @@ const drawStarisInterval = setInterval(() => {
     //changing ball collor for better to looks better when bouncing
     ball.strokeStyle = ballColor
     ball.fillStyle = ballColor
-    drawCircle();
-    // setInterval(draw, 1000/35); 
-  }
-}, 1000);
 
-function drawCircle() {
+    var BallMain = new Ball(x_start*25+7.5,(y_start-1) * 25 + 10)
+
+    setInterval(()=> {
+      drawCircle(BallMain)
+      if(BallMain.x < y*25){
+        BallMain = goDown(BallMain);
+        console.log(BallMain)
+      }
+    }, 500); 
+  }
+}, 10);
+
+function drawCircle(BallMain) {
   ball.beginPath();
-  ball.arc(x_start*25+7.5, (y_start-1) * 25 + 10, 10, 0, 2 * Math.PI);
+  ball.arc(BallMain.x, BallMain.y, 10, 0, 2 * Math.PI);
   ball.fill();
   ball.stroke()
-  ballMovement()
-}
-
-// Velocity x
-var vx = 5.0;
-// Velocity y - randomly set
-var vy;
-
-var gravity = 0.5;  
-var bounce = 0.7; 
-var xFriction = 0.1;
-
-function ballMovement() {
-  ball.x += vx;
-  ball.y += vy;
-  vy += gravity;
-
-  //If either wall is hit, change direction on x axis
-  if (ball.x + ball.radius !=  canvas.width || ball.x - ball.radius < 0) {
-    vx *= -1;
-  }
-
-  // Ball hits the floor
-  if (ball.y + ball.radius > canvas.height) {// ||
-
-    // Re-positioning on the base
-    ball.y = canvas.height - ball.radius;
-    //bounce the ball
-    vy *= -bounce;
-    //do this otherwise, ball never stops bouncing
-    if (vy < 0 && vy > -2.1)
-      vy = 0;
-    //do this otherwise ball never stops on xaxis
-    if (Math.abs(vx) < 1.1)
-      vx = 0;
-    xF();
-
-  }
-}
-
-function xF() {
-  if (vx > 0)
-    vx = vx - xFriction;
-  if (vx < 0)
-    vx = vx + xFriction;
-}
-
-function draw() {
-  ball.clearRect(0,0,canvas.width, canvas.height); 
-  //draw cirlce
-  ball.beginPath();
-  ball.arc(ball.x, ball.y, , 0, Math.PI*2, false);
-  ball.fillStyle = ball.color;
-  ball.fill();
   ball.closePath();
-  
-  ballMovement();
-
 }
+
+function goDown(BallMain){
+
+  while(BallMain.x< 100){
+    ball.clearRect(BallMain.x,BallMain.y,10, 2 * Math.PI);
+    BallMain = new Ball(BallMain.x+25,BallMain.y)
+    console.log(BallMain.y)
+    drawCircle(BallMain)
+  }
+
+  BallMain = new Ball(BallMain.x+25,BallMain.y)
+
+  while(BallMain.y< 25){
+    ball.clearRect(BallMain.x,BallMain.y,10, 2 * Math.PI);
+    BallMain = new Ball(BallMain.x,BallMain.y+25)
+    console.log(BallMain.y)
+    drawCircle(BallMain)
+  }
+
+  BallMain = new Ball(BallMain.x,BallMain.y+25)
+
+  return BallMain;
+}
+
+
+
+//! PHYSIC TESTING
+
+// // Velocity x
+// var vx = 5.0;
+// // Velocity y - randomly set
+// var vy;
+
+// var gravity = 0.5;  
+// var bounce = 0.7; 
+// var xFriction = 0.1;
+
+// function ballMovement(BallMain) {
+//   BallMain.x += vx;
+//   BallMain.y += vy;
+//   vy += gravity;
+
+//   //If either wall is hit, change direction on x axis
+
+//   for (const element of theArray) {
+//     // ...use `element`...
+//   }
+
+
+//   if (BallMain.x + BallMain.radius !=  canvas.width || BallMain.x - BallMain.radius < 0) {
+//     vx *= -1;
+//   }
+
+//   // Ball hits the floor
+//   if (BallMain.y + BallMain.radius > canvas.height) {// ||
+
+//     // Re-positioning on the base
+//     BallMain.y = canvas.height - BallMain.radius;
+//     //bounce the ball
+//     vy *= -bounce;
+//     //do this otherwise, ball never stops bouncing
+//     if (vy < 0 && vy > -2.1)
+//       vy = 0;
+//     //do this otherwise ball never stops on xaxis
+//     if (Math.abs(vx) < 1.1)
+//       vx = 0;
+//     if (vx > 0)
+//       vx = vx - xFriction;
+//     if (vx < 0)
+//       vx = vx + xFriction;
+
+//   }
+// }
